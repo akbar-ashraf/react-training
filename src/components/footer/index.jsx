b0270c8464d81c370fcd48cd84432cc29c095045
akbar-cn <akbar.ali@codeninjaconsulting.com>
2023-10-26 15:14:57 +0500

    footer link updated

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,16 @@ export const Footer = () => {
   return (
     <footer id="footer">
       <div className="container">
-        <p>Find me on: LinkedIn</p>
+        <p>
+          Find me on:
+          <a
+            href="https://pk.linkedin.com/in/akbar-ali-975693101"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </p>
       </div>
     </footer>
   );
