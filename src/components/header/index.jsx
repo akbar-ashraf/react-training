9ae4942ee5ef67eb7bec395637f5561f628967a7
akbar-cn <akbar.ali@codeninjaconsulting.com>
2023-11-03 11:45:04 +0500

    Task 104: Avoid props drilling finalized

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,8 @@
-export const Header = ({ isEditMode, onToggle }) => {
+import { useContext } from "react";
+import { EditModeContext } from "../../context/editModeContext";
+
+export const Header = ({ onToggle }) => {
+  const isEditMode = useContext(EditModeContext);
   return (
     <header id="header">
       <div className="container">
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,20 +3,19 @@ import { AboutSection } from "../../components/about";
 import { ExperienceSection } from "../../components/experience";
 import { Sidebar } from "../../components/sidebar";
 
-export const Home = ({ isEditMode }) => {
+export const Home = () => {
   const [experienceData, setExperienceData] = useState([]);
 
   return (
     <div className="portfolioContent">
       <div className="portfolioContentLeft">
-        <AboutSection isEditMode={isEditMode} experienceData={experienceData} />
+        <AboutSection experienceData={experienceData} />
         <ExperienceSection
-          isEditMode={isEditMode}
           experienceData={experienceData}
           setExperienceData={setExperienceData}
         />
       </div>
-      <Sidebar isEditMode={isEditMode} />
+      <Sidebar />
     </div>
   );
 };
